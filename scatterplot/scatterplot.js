b23ad536ad6eab22d2310f9fe553b1b8ba8d13ae
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-04-07 14:39:38 +0200

    set up sp plot dimensions

--- scatterplot/scatterplot.js
+++ scatterplot/scatterplot.js
@@ -1,5 +1,18 @@
 
 // Step 1: basic chart dimensions
+var chartWidth = 960,
+    chartHeight = 600;
+
+var margin = {top: 20, right: 40, bottom: 30, left: 40},
+    width = chartWidth - margin.left - margin.right,
+    height = chartHeight - margin.top - margin.bottom;
+
+var svg = d3.select("#scatterplot")
+    .attr("width", chartWidth)
+    .attr("height", chartHeight);
+
+var chart = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Step 2: load data
 
