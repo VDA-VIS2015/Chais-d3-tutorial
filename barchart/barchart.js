927b4943580ab82da389628b04641b9f7553ceb7
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-04-07 09:43:46 +0200

    configure plot sizes and margins

--- barchart/barchart.js
+++ barchart/barchart.js
@@ -2,6 +2,19 @@
 // D3 code for the bar chart
 
 // Step 2: set up plot size and margins
+var chartWidth = 960,
+    chartHeight = 600;
+ 
+var margin = {top: 20, right: 40, bottom: 30, left: 40},
+    width = chartWidth - margin.left - margin.right,
+    height = chartHeight - margin.top - margin.bottom;
+ 
+var svg = d3.select("#barchart")
+    .attr("width", chartWidth)
+    .attr("height", chartHeight);
+ 
+var chart = svg.append("g")
+   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Step 3: load data file
 
